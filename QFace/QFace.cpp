93e227868fdecc9767aae4eeadd90ffbc42ac2f8
pengfeituan-f18 <pengfeituan@gmail.com>
2014-05-25 03:10:40 +0800

    non err

--- QFace/QFace.cpp
+++ QFace/QFace.cpp
@@ -2,7 +2,7 @@
 #include<QtGui/QApplication>
 int main(int argc,char** argv){
     QApplication app(argc,argv);
-    QtView viewer=QtView(0);
+    QtView viewer(0);
     viewer.resize(640,480);
     viewer.show();
     return app.exec();
